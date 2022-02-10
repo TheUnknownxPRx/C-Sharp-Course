0aee7dc69956a48142c1d809f1ef6acd0fb7dbd1
Yael Laguna <77646834+YLaguna506@users.noreply.github.com>
2022-02-10 14:31:35 -0400

    Add files via upload

---
+++ Methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mike";
+            SayHi(name, 34);
+            SayHi("Tom", 12);
+            SayHi("Sam", 44);
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + ", you are " + age);
+        }
+
+    }
+}
