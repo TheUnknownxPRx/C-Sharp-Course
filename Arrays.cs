b29c1f8bca98cf28dfaf69e9237d914fe1bf970c
Yael Laguna <77646834+YLaguna506@users.noreply.github.com>
2022-02-10 10:27:00 -0400

    Add files via upload

---
+++ Arrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumber = {4, 8, 15, 16, 23, 42};
+            string[] friends = new string[10];
+            friends[0] = "Jim";
+            friends[1] = "Pam"; 
+
+            luckyNumber[1] = 16;
+
+            Console.WriteLine(luckyNumber[1]);
+            Console.WriteLine(friends[0]);
+
+            Console.ReadLine();
+        }
+    }
+}
