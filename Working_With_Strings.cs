c167ffa9e266529240ce8b95fa33f745bee7c8e8
Yael Laguna <77646834+YLaguna506@users.noreply.github.com>
2022-02-10 08:51:39 -0400

    Add files via upload

---
+++ Working_With_Strings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string phrase = "Demon " + "Slayers Corps";
+            Console.WriteLine(phrase);
+
+            Console.WriteLine(phrase.ToUpper() );
+            Console.WriteLine(phrase.ToLower() );
+            Console.WriteLine(phrase.Contains("Demon"));
+            Console.WriteLine(phrase[2]);
+
+            Console.WriteLine(phrase.IndexOf('S'));
+            Console.WriteLine(phrase.Substring(6,7));
+
+            // Prints lenght of string
+            Console.WriteLine(phrase.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
