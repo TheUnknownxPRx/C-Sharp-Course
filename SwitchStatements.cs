7fa3d5ef08f5e4d7456789374a509eb30c38d9ee
Yael Laguna <77646834+YLaguna506@users.noreply.github.com>
2022-02-10 15:04:59 -0400

    Add files via upload

---
+++ SwitchStatements.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getDay(1));
+
+            Console.ReadLine();
+        }
+
+        static string getDay(int dayNum)
+        {
+            string dayname;
+
+            switch (dayNum)
+            {
+                case 0:
+                    dayname = "Sunday";
+                    break;
+
+                case 1:
+                    dayname = "Monday";
+                    break;
+
+                case 2:
+                    dayname = "Tuesday";
+                    break;
+
+                case 3:
+                    dayname = "Wednesday";
+                    break;
+
+                case 4:
+                    dayname = "Thursday";
+                    break;
+
+                case 5:
+                    dayname = "Friday";
+                    break;
+
+                case 6:
+                    dayname = "Saturday";
+                    break;
+
+                default:
+                    dayname = "Invalid Day Number";
+                    break;
+
+            }
+
+            return dayname;
+        }
+
+    }
+}
