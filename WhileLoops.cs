b34091303a853eb08d527834e88607419709ae22
Yael Laguna <77646834+YLaguna506@users.noreply.github.com>
2022-02-10 15:10:01 -0400

    Add files via upload

---
+++ WhileLoops.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 6;
+            
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            while (index <= 5) ;
+
+                Console.ReadLine();
+        }
+
+        static string getDay(int dayNum)
+        {
+            string dayname;
+
+            switch (dayNum)
+            {
+                case 0:
+                    dayname = "Sunday";
+                    break;
+
+                case 1:
+                    dayname = "Monday";
+                    break;
+
+                case 2:
+                    dayname = "Tuesday";
+                    break;
+
+                case 3:
+                    dayname = "Wednesday";
+                    break;
+
+                case 4:
+                    dayname = "Thursday";
+                    break;
+
+                case 5:
+                    dayname = "Friday";
+                    break;
+
+                case 6:
+                    dayname = "Saturday";
+                    break;
+
+                default:
+                    dayname = "Invalid Day Number";
+                    break;
+
+            }
+
+            return dayname;
+        }
+
+    }
+}
