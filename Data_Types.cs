d4968c9c83bf2eef5305805311ad51d7a018dfb9
Yael Laguna <77646834+YLaguna506@users.noreply.github.com>
2022-02-10 08:35:08 -0400

    Add files via upload

---
+++ Data_Types.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string phrase = "La Casita";
+            char grade = 'A';
+            int age = 23;
+            double gpa = 3.7;
+            bool isMale = true;
+
+            Console.WriteLine(grade);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Variables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string charactername = "John";
+            int characterage;
+            characterage = 35;
+
+            Console.WriteLine("There once was a man named " + charactername);
+            Console.WriteLine("He was " + characterage + " years old.");
+
+            charactername = "Mike";
+            Console.WriteLine("He really liked the name " + charactername);
+            Console.WriteLine("But didn't like being " + characterage);
+
+            Console.ReadLine();
+        }
+    }
+}
