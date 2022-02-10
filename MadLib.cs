2fa043e77314158c672fdafc15f9596c8fb76948
Yael Laguna <77646834+YLaguna506@users.noreply.github.com>
2022-02-10 10:20:46 -0400

    Add files via upload

---
+++ MadLib.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralnoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralnoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralnoun + " are blue");
+            Console.WriteLine("I Love " + celebrity);
+
+            Console.ReadLine();
+        }
+    }
+}
