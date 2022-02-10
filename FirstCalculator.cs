42b611b342b2e8068a3b4d1bcf1735c62a4c4b5b
Yael Laguna <77646834+YLaguna506@users.noreply.github.com>
2022-02-10 09:37:54 -0400

    Add files via upload

---
+++ FirstCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Basic Sum Calculator
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.Readline());
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.Readline());
+
+            Console.WriteLine(num1 + num2);
+
+            Console.ReadLine();
+        }
+    }
+}
