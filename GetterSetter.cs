936bb926816c993a8d11e5625833f360c8b84202
Yael Laguna <77646834+YLaguna506@users.noreply.github.com>
2022-02-11 01:09:39 -0400

    Add files via upload

---
+++ GetterSetter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "Dog");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+
+            shrek.Rating = "R";
+
+            Console.WriteLine(avengers.Rating);
+
+            Console.ReadLine();
+        }
+
+    }
+}
