8c889ebf39407be4423e48e6ccc3b125cde8d153
Yael Laguna <77646834+YLaguna506@users.noreply.github.com>
2022-02-11 00:25:37 -0400

    Add files via upload

---
+++ Comments.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is a comment
+            // this prints something out
+            Console.WriteLine("Comments are fun");
+
+            /*
+            
+            everything here is a comment
+             
+             */
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
