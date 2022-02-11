3f23820cb4b87e104ca356e6969d1f74d0e7d9d1
Yael Laguna <77646834+YLaguna506@users.noreply.github.com>
2022-02-11 00:59:23 -0400

    Add files via upload

---
+++ ObjectMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student1 = new Student("Mike", "Biology", 2.6);
+            Student student2 = new Student("Yael", "Engineering", 3.7);
+
+            Console.WriteLine(student1.HasHonors());
+            Console.WriteLine(student2.HasHonors());
+
+            Console.ReadLine();
+        }
+
+    }
+}
