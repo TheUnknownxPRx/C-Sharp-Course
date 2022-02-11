a1eb0b482209f6b82acfde7a4ff8cf3ebb683409
Yael Laguna <77646834+YLaguna506@users.noreply.github.com>
2022-02-11 00:17:35 -0400

    Add files via upload

---
+++ ForLoops.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int x = 1; x <= 5; x++)
+            {
+                Console.WriteLine(x);
+            }
+
+            int[] luckyNumbers = { 4, 8, 16, 32, 64 };
+
+            for (int i = 1; i < luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
